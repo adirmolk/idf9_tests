af12c38d6a3afd149ec2de5ed48e7d6bc8dd455c
adirmolk <127691109+adirmolk@users.noreply.github.com>
2023-09-03 11:05:06 +0300

    Add files via upload

---
+++ db/mongoConnect.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(
+   process.env.MONGODB_LINK
+  );
+  // await mongoose.connect('mongodb+srv://koko9:MONKE@cluster0.jfgkq.mongodb.net/idf9');
+  console.log("mongo connect idf9 atlas");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
